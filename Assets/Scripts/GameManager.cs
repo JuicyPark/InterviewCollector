165ec5b2db16f6f495746c70368a91ff7e50685c
JuicyPark <bassoff@naver.com>
2020-06-01 19:21:37 +0900

    feat : Manager Binder

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    TouchController touchController;
+    QuestionManager questionManager;
+
+    void Start()
+    {
+        touchController = GetComponent<TouchController>();
+        questionManager = GetComponent<QuestionManager>();
+        BindEvents();
+    }
+
+    void OnDestroy()
+    {
+        UnBindEvents();
+    }
+
+    void BindEvents()
+    {
+        touchController.Touched += questionManager.ChangePanel;
+    }
+
+    void UnBindEvents()
+    {
+        touchController.Touched -= questionManager.ChangePanel;
+    }
+}

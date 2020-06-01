ec4922509175ec175e314a3d3277b6bb00b37028
JuicyPark <bassoff@naver.com>
2020-06-01 19:22:01 +0900

    feat : Touch Controller

---
+++ Assets/Scripts/TouchController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public enum Direction
+{
+    NONE, LEFT, RIGHT
+}
+
+public class TouchController : MonoBehaviour
+{
+    public Action<Direction> Touched;
+
+    Vector2 startTouch;
+    Vector2 endTouch;
+
+    [SerializeField]
+    float touchSensitive = 400f;
+
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            startTouch = Input.GetTouch(0).position;
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            endTouch = Input.GetTouch(0).position;
+            var currentDirection = GetDirection(endTouch - startTouch);
+            Touched?.Invoke(currentDirection);
+        }
+    }
+
+    Direction GetDirection(Vector3 directionVector)
+    {
+        if (Mathf.Abs(directionVector.x) < touchSensitive && Mathf.Abs(directionVector.y) < touchSensitive)
+            return Direction.NONE;
+
+        if (directionVector.x > 0)
+            return Direction.LEFT;
+        else
+            return Direction.RIGHT;
+    }
+
+}
